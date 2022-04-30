db55d234cdefbb9d5ff628d1c6351f18cdb70d68
LautaroZapata <oratualzc@gmail.com>
2022-04-29 22:54:55 -0300

    arreglos

--- js/countdown.js
+++ js/countdown.js
@@ -2,8 +2,8 @@ simplyCountdown('#cuenta', {
 	year: 2022, // required
 	month: 12, // required
 	day: 3, // required
-	hours: 00, // Default is 0 [0-23] integer
-	minutes: 00, // Default is 0 [0-59] integer
+	hours: 0, // Default is 0 [0-23] integer
+	minutes: 0, // Default is 0 [0-59] integer
 	seconds: 0, // Default is 0 [0-59] integer
 	words: { //words displayed into the countdown
 		days: 'Día',
